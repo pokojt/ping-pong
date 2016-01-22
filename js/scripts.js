553098d2e8e5b4b57b0088866a010a3513d69ca0
Taylor Pokoj <pokojt@gmail.com>
2016-01-21 18:17:56 -0800

    Add functionality to first spec test to return the range of numbers from 1 to the number chosen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
-var pingPong = function (number) {
-  var number = parseInt(number[0])
+var pingPong = function(inputNumber) {
+
   var rangeCount = [];
-  for (i = 0; i <= number.length; i++) {
-    list.push(i);
-}
+
+  for (var i = 1; i <= inputNumber; i++) {
+    rangeCount.push(i);
+  };
+  return rangeCount;
+};
 
 
-  return
+//   return
+//
+//   if (number % 3 === 0) {
+//     return "ping";
+//   }
+//   else if (number % 5 === 0) {
+//     return "pong";
+//   }
+//   else if (number % 15 === 0) {
+//     return "pingpong";
+//   }
+//   else {
+//     return "Please enter integers only!"
+//   }
+// }
 
-  if (number % 3 === 0) {
-    return "ping";
-  }
-  else if (number % 5 === 0) {
-    return "pong";
-  }
-  else if (number % 15 === 0) {
-    return "pingpong";
-  }
-  else {
-    return "Please enter integers only!"
-  }
-}
-
-$(document).ready(function() {
-  $("form#ping-pong").submit(function(event) {
-    var number = $("input#number").val();
-    var result = pingPong(number);
-
-    $(".number").text(number);
-    $("#result").show();
-
-    event.preventDefault();
-  });
-});
+// $(document).ready(function() {
+//   $("form#ping-pong").submit(function(event) {
+//     var number = $("input#number").val();
+//     var result = pingPong(number);
+//
+//     $(".number").text(number);
+//     $("#result").show();
+//
+//     event.preventDefault();
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,22 @@
 describe ('pingPong', function(){
   it ("returns a range of numbers from 1 to the number chosen", function (){
-    expect(pingPong(2).to.equal([1,2]));
-  });
-  it ("returns 'ping' if the entered number is divisable by 3", function (){
-    expect(pingPong(3).to.equal([1,2,"ping"]));
-  });
-  it ("returns 'ping' if the entered number is divisable by 5", function (){
-    expect(pingPong(5).to.equal([1,2,"ping",4,"pong"]));
-  });
-  it ("returns 'pingpong' if the entered number is divisable by 15", function (){
-    expect(pingPong(15).to.equal([1,2,"ping",4,"pong",6,7,8,"ping","pong",11,"ping",13,14,"pingpong"]));
+    expect(pingPong(2)).to.eql([1,2]);
   });
 });
+
+
+  // it ("returns 'ping' if the entered number is divisable by 3", function (){
+  //   expect(pingPong(3).to.equal([1,2,"ping"]));
+  // });
+  // it ("returns 'ping' if the entered number is divisable by 5", function (){
+  //   expect(pingPong(5).to.equal([1,2,"ping",4,"pong"]));
+  // });
+  // it ("returns 'pingpong' if the entered number is divisable by 15", function (){
+  //   expect(pingPong(15).to.equal([1,2,"ping",4,"pong",6,7,8,"ping","pong",11,"ping",13,14,"pingpong"]));
+  // });
+
+
+
 
 // describe ('pingPong', function(){
 //   it ("returns a range of numbers from 1 to the number chosen unless it is divisible by 3, 5, or 15", function (){
