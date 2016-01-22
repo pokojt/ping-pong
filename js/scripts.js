8fd54a7b34ab3f6d36d0e742340707b1017b6f46
Taylor Pokoj <pokojt@gmail.com>
2016-01-21 20:23:08 -0800

    Add functionality to all tests, replace multiples of 3, 5, and 15 with correct word. Test for 15 is not passing but the function still works for that inputed number in the console

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,25 @@ var pingPong = function(inputNumber) {
   var rangeCount = [];
 
   for (var i = 1; i <= inputNumber; i++) {
-    rangeCount.push(i);
+
+    if ((i % 5 === 0) && (i % 3 === 0)){
+      rangeCount.push("pingpong");
+    }
+    else if (i % 3 === 0) {
+      rangeCount.push("ping");
+    }
+    else if (i % 5 === 0) {
+      rangeCount.push("pong");
+    }
+    else {
+      rangeCount.push(i);
+    }
   };
   return rangeCount;
 };
 
 
-//   return
-//
-//   if (number % 3 === 0) {
-//     return "ping";
-//   }
-//   else if (number % 5 === 0) {
-//     return "pong";
-//   }
-//   else if (number % 15 === 0) {
-//     return "pingpong";
-//   }
-//   else {
-//     return "Please enter integers only!"
-//   }
-// }
+
 
 // $(document).ready(function() {
 //   $("form#ping-pong").submit(function(event) {
