5cc35855f0013f0111ced83a4c963d7b61925dd7
Taylor Pokoj <pokojt@gmail.com>
2016-01-22 07:53:19 -0800

    Add functionality and pass 15 spec. Forgot to change the 6 to ping

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ var pingPong = function(inputNumber) {
   for (var i = 1; i <= inputNumber; i++) {
 
     if (i % 15 === 0) {
-      rangeCount.push("pingpong");
-    }
-    else if (i % 3 === 0) {
-      rangeCount.push("ping");
+      rangeCount.push("ping pong");
     }
     else if (i % 5 === 0) {
       rangeCount.push("pong");
+    }
+    else if (i % 3 === 0) {
+      rangeCount.push("ping");
     }
     else {
       rangeCount.push(i);
--- spec/specs.js
+++ spec/specs.js
@@ -3,15 +3,15 @@ describe ('pingPong', function(){
     expect(pingPong(2)).to.eql([1, 2]);
   });
 
-  it ("returns 'pingpong' if the entered number is divisable by both three and 5", function (){
-    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", 6, 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]);
-  });
-
   it ("returns 'ping' if the entered number is divisable by 3", function (){
     expect(pingPong(3)).to.eql([1, 2, "ping"]);
   });
 
   it ("returns 'pong' if the entered number is divisable by 5", function (){
     expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"]);
+  });
+
+  it ("returns 'ping pong' if the entered number is divisable by 15", function () {
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping pong"]);
   });
 });
